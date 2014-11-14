cd58156f56b61462985d72e8714699e117039c8f
Mee Seong Im <MeeSeongIm@users.noreply.github.com>
2014-11-14 11:16:39 -0600

    Create hash_function.py

---
+++ hash_function.py
@@ -0,0 +1,11 @@
+
+import hashlib
+hash_object = hashlib.md5(b'This is a hash function, often used in cryptographic algorithms. This function takes a message and converts from bytes to a fixed-length sequence of symbols.')
+print(hash_object.hexdigest())
+
+
+
+# output: 66767cb3af8cbeccb49b81d4f2d75cac
+
+
+
